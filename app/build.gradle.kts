1aad6a0a0cc5bcf4fa73026b77cb0826ed11d0f8
beranju <beranjusihombing1@gmail.com>
2024-01-08 15:42:25 +0700

    feat: setup navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,10 @@ dependencies {
     // splash core
     implementation("androidx.core:core-splashscreen:1.0.1")
 
+    // navigation component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/synrgyacademy/finalproject/MainActivity.kt
+++ app/src/main/java/com/synrgyacademy/finalproject/MainActivity.kt
@@ -3,11 +3,23 @@ package com.synrgyacademy.finalproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ** setup bottom navigation
+        navView = findViewById(R.id.nav_view)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
+
     }
 }
