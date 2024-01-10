d7f7f7fbd4a8f1bbdc1e5db4f9dde632d5fa8fb7
beranju <beranjusihombing1@gmail.com>
2024-01-10 12:52:48 +0700

    feat: add penumpang's dialog

--- app/src/main/java/com/synrgyacademy/finalproject/ui/ticket/TicketFragment.kt
+++ app/src/main/java/com/synrgyacademy/finalproject/ui/ticket/TicketFragment.kt
@@ -1,24 +1,63 @@
 package com.synrgyacademy.finalproject.ui.ticket
 
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.synrgyacademy.finalproject.R
+import androidx.fragment.app.Fragment
 import com.synrgyacademy.finalproject.databinding.FragmentTicketBinding
+import com.synrgyacademy.finalproject.utils.DateUtils.getCurrentDateEEEDDMMM
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 
 class TicketFragment : Fragment() {
     private var _binding: FragmentTicketBinding? = null
     private val binding get() = _binding!!
+    private val calendar = Calendar.getInstance()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.tvSelectedDate.text = getCurrentDateEEEDDMMM()
+
+        binding.ivCalendar.setOnClickListener {
+            showDatePicker(
+                onSelectedDate = { binding.tvSelectedDate.text = it }
+            )
+        }
+        binding.ivCalendarBack.setOnClickListener {
+            showDatePicker(
+                onSelectedDate = { binding.tvSelectedDateBack.text = it }
+            )
+        }
         binding.mcPp.setOnCheckedChangeListener { _, isChecked ->
             binding.llDateBack.visibility = if (isChecked) View.VISIBLE else View.GONE
         }
+        binding.llPassenger.setOnClickListener {
+            PassengerDialogFragment().show(childFragmentManager, PassengerDialogFragment.TAG)
+        }
+    }
+
+    private fun showDatePicker(
+        onSelectedDate: (String) -> Unit
+    ) {
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _, year: Int, month: Int, dayOfMonth: Int ->
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(year, month, dayOfMonth)
+                val dateFormat = SimpleDateFormat("EEE, dd MMM", Locale.getDefault())
+                val formattedDate = dateFormat.format(selectedDate.time)
+                onSelectedDate(formattedDate)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
     }
 
     override fun onCreateView(
