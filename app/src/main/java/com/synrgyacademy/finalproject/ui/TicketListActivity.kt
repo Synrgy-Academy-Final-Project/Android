89c1c3bcdfe26c181815efd1b0327922e2d19b17
Kevin M <kevinmalikf@gmail.com>
2024-01-12 04:01:42 +0700

    feat: add TicketListActivity with their ui

---
+++ app/src/main/java/com/synrgyacademy/finalproject/ui/TicketListActivity.kt
@@ -0,0 +1,12 @@
+package com.synrgyacademy.finalproject.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.synrgyacademy.finalproject.R
+
+class TicketListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket_list)
+    }
+}
