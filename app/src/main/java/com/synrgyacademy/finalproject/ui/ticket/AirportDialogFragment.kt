14017f31948f34f675cd9d4a1838ee02f31f766f
beranju <beranjusihombing1@gmail.com>
2024-01-12 10:12:53 +0700

    feat: update cari tiket ui and add airport dialog

---
+++ app/src/main/java/com/synrgyacademy/finalproject/ui/ticket/AirportDialogFragment.kt
@@ -0,0 +1,33 @@
+package com.synrgyacademy.finalproject.ui.ticket
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.synrgyacademy.finalproject.R
+import com.synrgyacademy.finalproject.databinding.FragmentAirportDialogBinding
+
+
+class AirportDialogFragment : DialogFragment() {
+    private var _binding: FragmentAirportDialogBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAirportDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object{
+        const val TAG = "airport dialog"
+    }
+
+}
