6b849e44be7868c6275f54cfc77ceee99146ead2
Kevin M <kevinmalikf@gmail.com>
2024-01-21 15:38:01 +0700

    chore: setup theme, sent email to next fragment, update confirmation button logic

--- app/src/main/java/com/synrgyacademy/finalproject/ui/login/ForgotPasswordFragment.kt
+++ app/src/main/java/com/synrgyacademy/finalproject/ui/login/ForgotPasswordFragment.kt
@@ -1,23 +1,30 @@
 package com.synrgyacademy.finalproject.ui.login
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.synrgyacademy.finalproject.R
 import com.synrgyacademy.finalproject.databinding.FragmentForgotPasswordBinding
 import com.synrgyacademy.finalproject.utils.AuthUtil.isValidEmail
-import com.synrgyacademy.finalproject.utils.showAlert
+import com.synrgyacademy.finalproject.utils.showToast
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class ForgotPasswordFragment : DialogFragment() {
+    override fun getTheme(): Int {
+        return R.style.DialogTheme
+    }
+
     private var _binding: FragmentForgotPasswordBinding? = null
     private val binding get() = _binding!!
-    private val viewModel by viewModels<LoginViewModel>()
+    private val viewModel: LoginViewModel by viewModels<LoginViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,14 +39,35 @@ class ForgotPasswordFragment : DialogFragment() {
                 }
 
                 is ForgetPasswordState.Success -> {
+                    val email = binding.etEmail.text.toString().trim()
+                    val bundle = Bundle().apply {
+                        putString("email", email)
+                    }
+                    findNavController().navigate(R.id.confirmationOtpFragment, bundle)
+                    requireContext().showToast(
+                        getString(R.string.toast_otp_sent)
+                    )
                     dismiss()
-                    requireContext().showAlert(
-                        "Lupa Password",
-                        "Email telah di kirim, silah kan cek email anda",
-                        positiveCallback = { it.dismiss() })
                 }
             }
         }
+
+        // Disable the button initially
+        binding.btnSubmit.isEnabled = false
+
+        binding.etEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // No action needed here
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                // No action needed here
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                binding.btnSubmit.isEnabled = s.toString().trim().isNotEmpty()
+            }
+        })
 
         binding.btnSubmit.setOnClickListener {
             val email = binding.etEmail.text.toString().trim()
