4297480ea2a07ddead8f390273cc55652a27313b
Kevin M <kevinmalikf@gmail.com>
2024-02-04 03:26:52 +0700

    chore: set callback in ClassDialogFragment, integrate api in AirportDialogFragment

--- app/src/main/java/com/synrgyacademy/finalproject/ui/ticket/ClassDialogFragment.kt
+++ app/src/main/java/com/synrgyacademy/finalproject/ui/ticket/ClassDialogFragment.kt
@@ -1,7 +1,7 @@
 package com.synrgyacademy.finalproject.ui.ticket
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +10,47 @@ import com.synrgyacademy.finalproject.R
 import com.synrgyacademy.finalproject.databinding.FragmentClassDialogBinding
 
 class ClassDialogFragment : DialogFragment() {
+
+    // Define the interface
+    interface OnClassSelectedListener {
+        fun onClassSelected(selectedClass: String)
+    }
+
+    // Declare a variable of the interface type
+    private var listener: OnClassSelectedListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        // Set the listener to the parent fragment
+        listener = parentFragment as? OnClassSelectedListener
+        if (listener == null) {
+            throw ClassCastException("$parentFragment must implement OnClassSelectedListener")
+        }
+    }
+
     private var _binding: FragmentClassDialogBinding? = null
+
     private val binding get() = _binding!!
+
+    override fun getTheme(): Int {
+        return R.style.DialogTheme
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.rbGroup.setOnCheckedChangeListener { _, checkedId ->
+            val selectedClass = when (checkedId) {
+                R.id.rb_economic -> "Economic"
+                R.id.rb_economic_premium -> "Premium Economic"
+                R.id.rb_business -> "Business"
+                R.id.rb_main -> "First Class"
+                else -> ""
+            }
+            // Call the interface method to pass the selected class data
+            listener?.onClassSelected(selectedClass)
+            dismiss()
+        }
     }
 
     override fun onCreateView(
@@ -31,6 +67,6 @@ class ClassDialogFragment : DialogFragment() {
     }
 
     companion object {
-       const val TAG = "class dialog"
+        const val TAG = "class dialog"
     }
 }
