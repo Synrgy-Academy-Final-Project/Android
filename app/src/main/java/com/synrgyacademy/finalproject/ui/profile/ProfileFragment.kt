e953e61e74648348530c738cdedfcc8a2202fcbc
Kevin M <kevinmalikf@gmail.com>
2024-02-04 03:25:39 +0700

    chore: integrate api in ProfileFragment and TicketFragment

--- app/src/main/java/com/synrgyacademy/finalproject/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/synrgyacademy/finalproject/ui/profile/ProfileFragment.kt
@@ -5,56 +5,110 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.synrgyacademy.finalproject.R
+import com.synrgyacademy.finalproject.databinding.FragmentProfileBinding
+import com.synrgyacademy.finalproject.utils.showAlert
+import com.synrgyacademy.finalproject.utils.showToast
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel:ProfileViewModel by viewModels<ProfileViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.isLoginState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is IsLoginState.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+
+                is IsLoginState.Error -> {
+                    binding.progressBar.visibility = View.GONE
+
+                    requireContext().showToast("Terjadi kesalahan, silahkan coba lagi")
+                }
+
+                is IsLoginState.Success -> {
+                    binding.progressBar.visibility = View.GONE
+
+                    if(state.isLogin) showView(true) else showView(false)
                 }
             }
+        }
+
+        viewModel.logoutState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is AuthState.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+
+                is AuthState.Error -> {
+                    requireContext().showToast("Logout tidak berhasil, silahkan coba lagi")
+
+                    binding.progressBar.visibility = View.GONE
+                }
+
+                is AuthState.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    showView(false)
+
+                    findNavController().popBackStack(R.id.main_nav_graph, false)
+                    findNavController().navigate(R.id.profile_navigation)
+                }
+            }
+        }
+
+        binding.btnKeluar.setOnClickListener {
+            requireContext().showAlert(
+                title = getString(R.string.text_logout),
+                message = getString(R.string.text_logout_dialog_title),
+                positiveCallback = {
+                    lifecycleScope.launch {
+                        viewModel.logout()
+                    }
+                })
+        }
+
+        binding.btnMasuk.setOnClickListener {
+            findNavController().navigate(R.id.loginFragment)
+        }
+    }
+
+    private fun showView(isLogin: Boolean) {
+        binding.cvNotLogin.visibility = if (isLogin) View.GONE else View.VISIBLE
+
+        binding.llTicketConfiguration.visibility = if (isLogin) View.VISIBLE else View.GONE
+        binding.llUserHelp.visibility = if (isLogin) View.VISIBLE else View.GONE
+
+        binding.tvAnggotaFly.visibility = if (isLogin) View.VISIBLE else View.GONE
+        binding.btnEditProfil.visibility = if (isLogin) View.VISIBLE else View.GONE
+        binding.tvName.visibility = if (isLogin) View.VISIBLE else View.GONE
+        binding.tvEmail.visibility = if (isLogin) View.VISIBLE else View.GONE
+
+        binding.btnKeluar.visibility = if (isLogin) View.VISIBLE else View.GONE
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
