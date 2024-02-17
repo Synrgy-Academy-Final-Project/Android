8e40cfb67052db598e50f352c82f972f4ae58f00
Kevin M <kevinmalikf@gmail.com>
2024-02-17 20:23:22 +0700

    chore: delete notification library and used native notification

--- app/src/main/java/com/synrgyacademy/finalproject/utils/NotificationUtils.kt
+++ app/src/main/java/com/synrgyacademy/finalproject/utils/NotificationUtils.kt
@@ -1,34 +1,57 @@
 package com.synrgyacademy.finalproject.utils
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.media.RingtoneManager
+import android.graphics.Color
+import android.os.Build
+import android.provider.Settings
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.synrgyacademy.finalproject.R
-import io.karn.notify.Notify
 
+@SuppressLint("MissingPermission")
 object NotificationUtils {
-    fun createNotification(context: Context, header: String, title: String, text: String, activityClass: Class<*>) {
-        Notify
-            .with(context)
-            .header {
-                icon = R.mipmap.ic_rounded
-                headerText = header
+    fun createHeadsUpNotification(context: Context, title: String, text: String, activityClass: Class<*>) {
+        val channelId = "heads_up_notification"
+        val notificationId = 1
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Heads Up Notification"
+            val descriptionText = "This is a heads up notification"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
             }
-            .meta { // this: Payload.Meta
-                // Launch the specified activity once the notification is clicked.
-                clickIntent = PendingIntent.getActivity(context,
-                    0,
-                    Intent(context, activityClass),
-                    PendingIntent.FLAG_IMMUTABLE)
-            }
-            .content { // this: Payload.Content.Default
-                this.title = title
-                this.text = text
-            }
-            .alerting("default") {
-                sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-            }
-            .show()
+
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val intent = Intent(context, activityClass)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent,
+            PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.mipmap.ic_rounded)
+            .setColor(ContextCompat.getColor(context, R.color.primary_blue))
+            .setContentTitle(title)
+            .setContentText(text)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(notificationId, builder.build())
+        }
     }
 }
